bcdc291b4d3bf9e8c942cebafdc19028ed2d9e73
Aanish Pradhan <aanish.pradhan@gmail.com>
2023-11-20 19:29:35 -0500

    WIP: Calibration frame subtraction code

---
+++ src/Calibration/calibrate.cu
@@ -0,0 +1,31 @@
+/**
+ * Implements functions for calibration frame subtraction. 
+ * 
+ * @author Aanish Pradhan
+ * @version 2023-11-20
+ */
+
+// INCLUDE LIBRARIES
+#include "calibrate.h"
+#include "calibrate_kernels.cu"
+
+// FUNCTIONS
+Stack* subtractBDF(bool haveBiasFrames, Stack* biasFrames, bool haveDarkFrames, 
+	Stack* darkFrames, bool haveFlatFrames, Stack* flatFrames, 
+		Stack* lightFrames)
+{
+	
+	
+	
+	
+	
+	
+	
+	
+	// Check if 
+	if (haveBiasFrames == true)
+	{
+		
+	}
+
+}
---
+++ src/Calibration/calibrate_kernels.cu
@@ -0,0 +1,12 @@
+/**
+ * Implements DEVICE kernels for calibration frame subtraction.
+ * 
+ * @author Aanish Pradhan
+ * @version 2023-11-20
+ */
+
+// INCLUDE LIBRARIES
+#include "../Stacking/stack.h"
+#include <math.h>
+
+// KERNELS
