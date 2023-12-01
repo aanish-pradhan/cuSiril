78960517b31cfe3b893d3e0baea3311b72cd8e01
Aanish Pradhan <aanish.pradhan@gmail.com>
2023-12-01 03:24:33 -0500

    WIP: Calibration frame subtraction functions and kernels

--- src/Calibration/calibrate.cu
+++ src/Calibration/calibrate.cu
@@ -2,30 +2,63 @@
  * Implements functions for calibration frame subtraction. 
  * 
  * @author Aanish Pradhan
- * @version 2023-11-20
+ * @version 2023-11-28
  */
 
 // INCLUDE LIBRARIES
 #include "calibrate.h"
 #include "calibrate_kernels.cu"
+#include <math.h>
+
+// DEFINITIONS
 
 // FUNCTIONS
-Stack* subtractBDF(bool haveBiasFrames, Stack* biasFrames, bool haveDarkFrames, 
-	Stack* darkFrames, bool haveFlatFrames, Stack* flatFrames, 
+Stack* calibrate(bool useBiasFrames, Stack* biasFrames, bool useDarkFrames, 
+	Stack* darkFrames, bool useFlatFrames, Stack* flatFrames, 
 		Stack* lightFrames)
 {
-	
-	
-	
-	
-	
-	
-	
-	
-	// Check if 
-	if (haveBiasFrames == true)
-	{
+	if (useBiasFrames || useDarkFrames || useFlatFrames)
+	{	
 		
+		
+		
+		
+		
+		
+		// Normalize Light frames
+
+
+
+		// Check if Bias frames are provided
+		if (useBiasFrames == true)
+		{
+			// TODO: Stack Bias frames with specified method
+			// TODO: Subtract master Bias frame from Light frames
+		}
+
+		// Check if Dark frames are provided
+		if (useDarkFrames == true)
+		{
+			if (useBiasFrames == true)
+			{
+				// TODO: Subtract master Bias frame from Dark frames
+			}
+			// TODO: Stack (corrected) Dark frames with specified method
+			// TODO: Subtract master Dark frame from Light frames 
+		}
+
+		// Check if Flat frames are provided
+		if (haveFlatFrames == true)
+		{
+			if (haveBiasFrames == true)
+			{
+				// TODO: Subtract master Bias frame from Flat frames
+			}
+			// TODO: Stack (corrected) Flat frames with specified method
+			// TODO: Divide Light frames by master Flat frame
+		}
 	}
 
+	return lightFrames;
 }
+
--- src/Calibration/calibrate_kernels.cu
+++ src/Calibration/calibrate_kernels.cu
@@ -2,11 +2,59 @@
  * Implements DEVICE kernels for calibration frame subtraction.
  * 
  * @author Aanish Pradhan
- * @version 2023-11-20
+ * @version 2023-12-01
  */
 
 // INCLUDE LIBRARIES
-#include "../Stacking/stack.h"
+#include "calibrate.cuh"
 #include <math.h>
 
 // KERNELS
+/**
+ * Normalizes a stack of light frames by UINT16_MAX.
+ * 
+ * @param lightFrames Pointer to a Stack with unnormalized, uint16 light frames
+ */
+__global__ void normalizeLightFrames(Stack* lightFrames)
+{
+	// 2D thread indexes
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	/*
+	The subframes are flattened so we convert the 2D thread indexes into a 
+	linear, 1D index.
+	*/
+	int pixelIndex = lightFrames->imageWidth * y + x;
+
+	if (x < lightFrames->imageWidth && y < lightFrames->imageHeight)
+	{
+		for (uint64_t subframe = 0; subframe < lightFrames->numberOfSubframes; 
+			subframe++)
+		{
+			/*
+			The subframes are flattened so we offset pixelIndex to move to the 
+			next subframe in the stack.
+			*/
+			uint64_t subframeOffset = lightFrames->pixelsPerImage * subframe;
+
+			lightFrames->redSubframes[pixelIndex] /= UINT16_MAX;
+			lightFrames->greenSubframes[pixelIndex] /= UINT16_MAX;
+			lightFrames->blueSubframes[pixelIndex] /= UINT16_MAX;
+		}
+	}
+}
+
+/**
+ * Subtracts a master calibration frame from other calibration frames or light
+ * frames.
+ * 
+ * @param parentFrame The calibration frames or light frames to be subtracted
+ * from
+ * @param calibrationFrame The calibration frame to subtract
+ */
+__global__ void subtractMasterCalibrationFrame(Stack* parentFrame, 
+	Stack* calibrationFrame)
+{
+	// TODO
+}
